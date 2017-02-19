fc8db880529fb8da0b8673d48990e544c67196d4
Paul <mr.weddy@gmail.com>
2017-02-20 00:55:02 +0200

    F commit

---
+++ src/main/java/com/clan/DemaApplication.java
@@ -0,0 +1,12 @@
+package com.clan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemaApplication.class, args);
+	}
+}
